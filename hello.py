c2ddddd9c60d3ecb10521d63b134699d149b7199
KitKat0098 <catherine.sijo@marcellin.school.nz>
2025-03-04 10:29:39 +1300

    Added a input statement

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
 print("Hello, world!")
 print("My favourite colour is purple")
+#This code welcomes a person to wonderland
+name = input("What is your name? ")
+print(f"Welcome to wonderland {name}!")
