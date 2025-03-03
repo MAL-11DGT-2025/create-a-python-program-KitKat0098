f18800cd172f9871f932305b12dfc6e7bfb4db73
KitKat0098 <catherine.sijo@marcellin.school.nz>
2025-03-04 10:39:31 +1300

    Asks for the users age

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 print("Hello, world!")
 print("My favourite colour is purple")
+
 #This code welcomes a person to wonderland
 name = input("What is your name? ")
 print(f"Welcome to wonderland {name}!")
-#This code asks user for nage and calls them old
+
+#This code asks user for name and calls them old
 age = input("How old are you? ")
-print(f"Wow! {age} is old!")
+print(f"Wow! {age} is old! You are so very old!")
