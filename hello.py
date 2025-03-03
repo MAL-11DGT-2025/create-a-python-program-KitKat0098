3d842f6e7becf43a783078cb0621977518fd24d2
KitKat0098 <catherine.sijo@marcellin.school.nz>
2025-03-04 10:56:51 +1300

    Calls the user old

--- hello.py
+++ hello.py
@@ -6,5 +6,8 @@ name = input("What is your name? ")
 print(f"Welcome to wonderland {name}!")
 
 #This code asks user for name and calls them old
-age = input("How old are you? ")
+age = int(input("How old are you? "))
 print(f"Wow! {age} is old! You are so very old!")
+
+if age > 25:
+    print("You are old")
