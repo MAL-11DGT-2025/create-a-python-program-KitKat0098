d8a7a7c354b63240b4f6b682578e5db1db56e3b3
KitKat0098 <catherine.sijo@marcellin.school.nz>
2025-03-07 12:53:45 +1300

    Worked with some list functions

---
+++ lists.py
@@ -0,0 +1,14 @@
+colours = ["Red","Green","Purple","Yellow","Cyan"]
+#print(colours[0])
+#print(colours[1])
+
+#for i in colours:
+#   print(i)
+#print(colours[-1])
+#print(colours[-3])
+
+#print(len(colours))
+
+for i in colours:
+    if i == "Yellow":
+        print("There is yellow")
