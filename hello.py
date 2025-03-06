5fbf7c1aacbe7b112cb988e03f4d57dec9efdc53
KitKat0098 <catherine.sijo@marcellin.school.nz>
2025-03-06 14:44:32 +1300

    Added code for asking age and commented code

--- hello.py
+++ hello.py
@@ -1,16 +1,28 @@
-print("Hello, world!")
-print("My favourite colour is purple")
+#print("Hello, world!")
+#print("My favourite colour is purple")
 
 #This code welcomes a person to wonderland
-name = input("What is your name? ")
-print(f"Welcome to wonderland {name}!")
+#name = input("What is your name? ")
+#print(f"Welcome to wonderland {name}!")
 
 #This code asks user for name and calls them old
-age = int(input("How old are you? "))
-print(f"Wow! {age} is old! You are so very old!")
+#age = int(input("How old are you? "))
+#print(f"Wow! {age} is old! You are so very old!")
 
-if age > 25:
-    print("You are old")
+#if age > 25:
+ #   print("You are old")
     #This statement calls the person old if theyre older than 25
-if age >= 13 or age <= 19:
-    print("You are a teenager")
+#if age >= 13 and age <= 19:
+ #   print("You are be a teenager")
+#if age >= 13 or age <= 19:
+ #   print("You might be a teenager")
+#umbrella = False
+#sunny = True
+#if umbrella and sunny:
+#    print("You think it's going to rain.")
+#elif not umbrella and sunny:
+#        print("You think you dont need an umbrella.")
+#elif umbrella and not sunny:
+#    print("It must be raining.")
+#elif not umbrella and not sunny:
+#    print("You must be drenched.")
